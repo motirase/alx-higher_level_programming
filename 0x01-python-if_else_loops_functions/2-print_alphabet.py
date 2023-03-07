95c5eb49ccdb3e0a5b6eb0223d6cfb45941ca5fc
motirase <117908555+motirase@users.noreply.github.com>
2023-03-07 14:49:30 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:s}".format(alpha_letters), end="")
+for a in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(a)), end="")
