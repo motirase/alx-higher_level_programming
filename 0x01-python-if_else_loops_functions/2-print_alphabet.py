2c55e79801b21fddfecebf08820dca0ddb05d3f3
motirase <motirase2022@gmail.com>
2023-03-07 03:45:11 -0800

    third task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
-for ch in range(97, 123):
-    print(chr(ch))
-
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 'e' or alpha_letters == 'q':
+       continue
+    print("{:c}".format(alpha_letters), end="")
