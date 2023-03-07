022572f1874136be648a9060f309e299574f2ce8
motirase <117908555+motirase@users.noreply.github.com>
2023-03-07 14:46:57 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 for alpha_letters in range(ord('a'), ord('z')+1):
     if alpha_letters == 'e' or alpha_letters == 'q':
        continue
-    print("{:c}".format(alpha_letters), end="")
+    print("{:s}".format(alpha_letters), end="")
