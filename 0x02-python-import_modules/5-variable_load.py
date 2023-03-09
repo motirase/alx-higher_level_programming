a1ae811390a18266c7b0c17e4e24eb6c71dc0693
motirase <117908555+motirase@users.noreply.github.com>
2023-03-09 13:13:21 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+    from variable_load_5 import a
+    print("{:d}".format(a))
