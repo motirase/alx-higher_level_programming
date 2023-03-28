e158fdb8b4d115e6257ca398f6a6770810e01781
motirase <117908555+motirase@users.noreply.github.com>
2023-03-28 14:51:05 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    """Empty class that defines a square"""
-    pass
+# 0-square.py
+# wakjira Bekele <wakjirabekele2018@gmail.com>
+"""Define a class Square."""
 
+
+class Square:
+    """Represent a square."""
+    pass
