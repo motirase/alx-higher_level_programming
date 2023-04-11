9ee99e377f02f5265121646521a17bc3676a74e6
motirase <117908555+motirase@users.noreply.github.com>
2023-04-11 15:06:31 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 100-append_after.py
-# wakjira Bekele <wakjirabekele2018@gmial.com>
+# Megersa Oljira <motirase2022@gmial.com>
 """Defines a text file insertion function."""
 
 
