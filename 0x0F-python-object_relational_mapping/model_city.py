4b0903664a440cd67c54eb53b79754a1863d544f
motirase <117908555+motirase@users.noreply.github.com>
2023-06-20 15:59:05 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Script prints all City objects
-Takes three arguments
-    mysql username
-    mysql password
-    database name
-Connects to host localhost and default port (3306)
 """
-if __name__ == "__main__":
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import aliased, sessionmaker
-    from model_state import Base, State
-    from model_city import City
-    from sys import argv
-    Session = sessionmaker()
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    session = Session(bind=engine)
-    Base.metadata.create_all(engine)
-    result = (session.query(State.name, City.id, City.name).filter(
-        State.id == City.state_id).order_by(City.id).all())
-    for i in result:
-        print("{}: ({:d}) {}".format(i[0], i[1], i[2]))
-    session.close()
+Python file that contains the class definition of a State and an instance
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that inherits from Base
+
+    Attributes:
+        id: Id state
+        name: Name of state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
